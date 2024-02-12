72b70c8a0ac5d2fa411bfb1b505e776c7e92e0a7
Faith <wachiraafaith@gmail.com>
2024-02-12 07:49:55 +0300

    Add files

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+fun main() {
+    var fullName: String ="Faith"
+    println(fullName)
+    var age: Int = 21
+    println(age)
+    var weightInKg: Double = 57.4
+    println(weightInKg)
+    var citizen: Boolean = true
+    println(citizen)
+
+}
